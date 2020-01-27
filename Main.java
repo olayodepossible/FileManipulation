b6ea61cc9a4c7afb8a0332568a0279e26a9c8ce9
Akinfemiwa Abayomi Olayode <olayodepossible@gmail.com>
2020-01-27 10:19:56 +0100

    filtering condition included

--- Main.java
+++ Main.java
@@ -9,23 +9,26 @@ public class Main {
 
 	public static void main(String[] args)throws Exception {
 		Map<String, String> list1 = new HashMap<>();
-		List<String[]> fileList = new ArrayList<>();
+		List<String[]> fileList;
 
 		if(args.length < 1){
 			fileList = configFile("src/config.txt");
 			for(int i = 0; i < fileList.size(); i++){
 				String[] s1 =fileList.get(i);
+				if(list1.containsKey(s1[0])) continue;
 				for(int j = 0; j < s1.length -1; j++){
 					list1.put(s1[0], s1[1]);
 				}
 			}
 		}
-
-		fileList = configFile(args[0]);
-		for(int i = 0; i < fileList.size(); i++){
-			String[] s1 =fileList.get(i);
-			for(int j = 0; j < s1.length -1; j++){
-				list1.put(s1[0], s1[1]);
+		else {
+			fileList = configFile(args[0]);
+			for(int i = 0; i < fileList.size(); i++){
+				String[] s1 =fileList.get(i);
+				if(list1.containsKey(s1[0])) continue;
+				for(int j = 0; j < s1.length -1; j++){
+					list1.put(s1[0], s1[1]);
+				}
 			}
 		}
 
@@ -33,18 +36,32 @@ public class Main {
 	}
 
 	public static List<String[]> configFile(String file) throws Exception{
-		ArrayList<String[]> s1 = new ArrayList<>();
+		ArrayList<String[]> listOfData = new ArrayList<>();
+		final String APPLICATION = "application.";
 		try(BufferedReader input = new BufferedReader(new FileReader("src/config.txt"));){
 			String eachLine;
+			boolean flag = false;
 			while((eachLine = input.readLine()) != null){
-				s1.add(eachLine.split("="));
+				if(eachLine.equals("[application]")){
+					flag = true;
+					continue;
+				}
+				if(eachLine.equals("")){
+					flag = false;
+					continue;
+				}
+				String[] split = eachLine.split("=");
+				if(flag){
+					split[0] = APPLICATION + split[0];
+				}
+				listOfData.add(split);
 			}
 		}
 		catch (FileNotFoundException e){
 			System.out.println(e.getMessage());
 		}
 
-		return s1;
+		return listOfData;
 	}
 
 }
